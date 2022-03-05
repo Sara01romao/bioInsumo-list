36e714419e74c08117d7bf523b68f5778c2aff1c
Sara Romao <sara-romao@live.com>
2022-03-05 12:23:29 -0300

    ajuste estilos

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -6,7 +6,7 @@ import { Link, NavLink } from 'react-router-dom';
 const Header = () => {
   return (
     <nav>
-        <Link to="/">BioInsumos</Link>
+        <Link to="/" className='logo'>BioInsumos</Link>
         <ul>
             <li><NavLink to="/convencional" className="link">Agricultura Convencional</NavLink></li>
             <li><NavLink to="/organico" className="link">Agricultura Orgânica</NavLink></li>
