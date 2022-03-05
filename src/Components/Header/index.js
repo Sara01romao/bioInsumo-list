70dcaf77ec1dbac65e0c08a9bdd645c24ca136dc
Sara Romao <sara-romao@live.com>
2022-03-05 00:03:37 -0300

    Header

---
+++ src/Components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+
+
+const Header = () => {
+  return (
+    <nav>
+        <Link to="/">BioInsumos</Link>
+        <ul>
+            <li><NavLink to="/convencional" className="link">Agricultura Convencional</NavLink></li>
+            <li><NavLink to="/organico" className="link">Agricultura Orgânica</NavLink></li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Header ; 
